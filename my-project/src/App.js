8da5003965d6d3f331ca3e1521488e085bbd0df2
apollo920 <apollo.sa@luscastechnologyservice.com>
2024-10-28 13:49:46 -0300

    building components

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import HelloWorld from './components/HelloWorld';
 function App() {
-  return (
+  const newName = 'Daquele Jeitão';
+  function sum(a , b) {
+    return a + b;
+  }
+  const url = 'https://via.placeholder.com/150'
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(2,3)} </p>
+      <img src={url} alt='Minha Imagem'/>
+      <HelloWorld />
     </div>
-  );
+
+
+      )
+  
 }
 
 export default App;
